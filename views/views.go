ee37ffb668698055ccf59fffe6710b5e3d026238
PimmyTrousers <pim.trouerbach1@gmail.com>
2016-11-12 14:21:50 -0800

    Added a navigation bar to the webapp

--- views/views.go
+++ views/views.go
@@ -4,8 +4,9 @@ import "html/template"
 
 func NewView(layout string, files ...string) *View { // this is called a variadric parameter, it means that it takes in any numbber of args as long as it is a string the *View to the next of the params that are passed is the
 	files = append(files,
-		"views/layout/footer.gohtml",
-		"views/layout/bootstrap.gohtml")
+		"views/layouts/footer.gohtml",
+		"views/layouts/bootstrap.gohtml",
+		"views/layouts/navbar.gohtml")
 	t, err := template.ParseFiles(files...)
 	if err != nil {
 		panic(err)
